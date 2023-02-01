9491fcdf4b448e96574b2c8602c6fdadc2a21061
MINEDUCYT <lunaacosta>
2023-01-31 22:05:17 -0600

    Agrego Semana3

---
+++ Semana3/primeraApp/src/views/EjemploView.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <example-component title="Programacion Computacional IV"></example-component>
+</template>
+
+<script>
+import exampleComponent from '@/components/EjemploComponente.vue'
+export default {
+    name:"LunaAcosta",
+    components:{
+        exampleComponent,
+    }
+}
+</script>
